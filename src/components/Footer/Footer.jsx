344611ae9ce5a629700097177fdd01c3aae8581b
Wilder Ap <wilderasprilla321@gmail.com>
2024-07-16 16:25:16 -0500

    fix laguages to spanish and social buttons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,26 +1,31 @@
-import data from '../../data.json';
-import CustomLink from '../ui/CustomLink';
-import Logo from '../ui/Logo';
+import data from "../../data.json";
+import SocialButtons from "../Socials";
+import CustomLink from "../ui/CustomLink";
+import Logo from "../ui/Logo";
 
 const Footer = () => {
-  const { links } = data['section-nav'];
-  const { text } = data['section-footer'];
+  const { links } = data["section-nav"];
+  const { text } = data["section-footer"];
 
   return (
-    <footer className='footer footer-center p-10 bg-custom-neutral text-base-content rounded'>
+    <footer className="footer footer-center p-10 bg-custom-neutral text-base-content rounded">
       <div>
-        <Logo colorText='text-custom-silver' />
+        <Logo colorText="text-custom-silver" />
       </div>
-      <nav className='flex flex-wrap gap-4 justify-center'>
+      <nav className="flex flex-wrap gap-4 justify-center">
         {links.map((link) => (
           <CustomLink
             text={link.text}
             href={link.href}
             key={link.id}
-            colorText='text-custom-silver'
+            colorText="text-custom-silver"
           />
         ))}
       </nav>
+
+      <div>
+        <SocialButtons />
+      </div>
 
       <aside>
         <p>
