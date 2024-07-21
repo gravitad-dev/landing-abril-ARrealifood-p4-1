5bdc495a1623f8cc94e81cdc162eb6e8f13bd507
Benjamín <benser22@hotmail.com>
2024-07-20 23:31:48 -0300

    refactor(landing): links to social media and contact elements changed

--- src/components/Navbar/MenuDesktop.jsx
+++ src/components/Navbar/MenuDesktop.jsx
@@ -3,14 +3,14 @@ import CustomLink from '../ui/CustomLink';
 
 const MenuDesktop = ({ data, scrolled }) => {
   return (
-    <nav className='w-full hidden lg:flex lg:justify-between'>
+    <nav className="w-full hidden lg:flex lg:justify-between">
       <ul
         className={`
        flex items-center mx-auto gap-2
         `}
       >
         {data.links.map((link) => (
-          <li key={link.id} className='p-2'>
+          <li key={link.id} className="p-2">
             <CustomLink
               {...link}
               colorText={`
@@ -20,8 +20,8 @@ const MenuDesktop = ({ data, scrolled }) => {
           </li>
         ))}
       </ul>
-      <CustomBtn className='btn'>
-        <CustomLink {...data.button} />
+      <CustomBtn className="btn">
+        <CustomLink {...data.button} isExternal={'true'} />
       </CustomBtn>
     </nav>
   );
