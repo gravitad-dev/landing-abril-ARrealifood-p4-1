f75c146920a7b6aa27e61ac5f09be7be7bcece63
silvak <silvak.jeg@gmail.com>
2024-07-25 12:10:10 -0400

    change: remove info section

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import Blog from './components/Blog/Blog';
-import Clients from './components/Clients/Clients';
-import Contact from './components/Contact/Contact';
-import Footer from './components/Footer/Footer';
-import Hero from './components/Hero/Hero';
-import Info from './components/Info/Info';
-import Membership from './components/Membership/Membership';
-import Navbar from './components/Navbar/Navbar';
-import Projects from './components/Projects/Projects';
-import Stats from './components/Stats/Stats';
-import Layout from './components/ui/Layout';
+import Blog from "./components/Blog/Blog";
+import Clients from "./components/Clients/Clients";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import Hero from "./components/Hero/Hero";
+import Info from "./components/Info/Info";
+import Membership from "./components/Membership/Membership";
+import Navbar from "./components/Navbar/Navbar";
+import Projects from "./components/Projects/Projects";
+import Stats from "./components/Stats/Stats";
+import Layout from "./components/ui/Layout";
 
 function App() {
   return (
-    <main className='min-h-screen bg-[#fafafa]'>
+    <main className="min-h-screen bg-[#fafafa]">
       <Navbar />
       <Hero />
-      <Layout maxWidth='max-w-[1440px]'>
+      <Layout maxWidth="max-w-[1440px]">
         <Clients />
         <Membership />
         <Stats />
-        <Blog />
+        {/* <Blog /> */}
         <Info />
         <Projects />
         <Contact />
--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,27 +1,27 @@
-import { FaInstagram } from 'react-icons/fa';
-import { FaFacebook } from 'react-icons/fa';
-import { FaLinkedin } from 'react-icons/fa';
-import { FaXTwitter } from 'react-icons/fa6';
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 const social = [
   {
     id: 0,
-    url: 'https://www.instagram.com/dataminder1/',
+    url: "https://www.instagram.com/dataminder1/",
     icon: <FaInstagram />,
   },
   {
     id: 1,
-    url: 'https://www.facebook.com/profile.php?id=61561155863586',
+    url: "https://www.facebook.com/profile.php?id=61561155863586",
     icon: <FaFacebook />,
   },
   {
     id: 2,
-    url: 'https://www.linkedin.com/company/103613287/admin/dashboard/',
+    url: "https://www.linkedin.com/company/103613287/admin/dashboard/",
     icon: <FaLinkedin />,
   },
   {
     id: 3,
-    url: 'https://x.com/Dataminder12',
+    url: "https://x.com/Dataminder12",
     icon: <FaXTwitter />,
   },
 ];
@@ -30,14 +30,14 @@ function SocialButtons() {
   return (
     <div className="flex py-10 justify-center">
       <ul>
-        <li className="flex gap-[60px]">
+        <li className="flex gap-[16px]">
           {social.map((item) => (
             <a
               key={item.id}
               href={item.url}
               target="_blank"
               rel="noreferrer"
-              className="hover:bg-[#474748] bg-gray-800 h-[28px] w-[28px] rounded-[4px] text-white text-[16px] flex justify-center items-center"
+              className=" h-[28px] w-[28px] rounded-[4px] text-white text-[16px] flex justify-center items-center"
             >
               {item.icon}
             </a>
